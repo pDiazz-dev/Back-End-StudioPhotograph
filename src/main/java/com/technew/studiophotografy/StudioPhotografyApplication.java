3a25214f50383b7f8f5bd2823410746a5e7afab3
pDiazz-dev <pedrodias20152015@gmail.com>
2026-02-05 16:17:19 -0300

    inicio do projeto

---
+++ src/main/java/com/technew/studiophotografy/StudioPhotografyApplication.java
@@ -0,0 +1,13 @@
+package com.technew.studiophotografy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudioPhotografyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudioPhotografyApplication.class, args);
+    }
+
+}
