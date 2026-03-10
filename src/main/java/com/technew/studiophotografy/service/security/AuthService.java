5c8b4a129ed6ae123a2e26e9075a038f6f178b01
Pedro <pedroestudos20252025@gmail.com>
2026-03-10 17:13:02 -0300

    feat(AuthService):add generate refresh token

--- src/main/java/com/technew/studiophotografy/service/security/AuthService.java
+++ src/main/java/com/technew/studiophotografy/service/security/AuthService.java
@@ -4,15 +4,20 @@ import com.technew.studiophotografy.entity.user.DTOs.AuthUserDTO;
 import com.technew.studiophotografy.entity.user.DTOs.RegisterDTO;
 import com.technew.studiophotografy.entity.user.TypeRole;
 import com.technew.studiophotografy.entity.user.Users;
+import com.technew.studiophotografy.repository.RefreshTokenRepository;
 import com.technew.studiophotografy.repository.RolesRepository;
 import com.technew.studiophotografy.repository.UserRepository;
 import com.technew.studiophotografy.service.security.DTOs.LoginResponse;
+import com.technew.studiophotografy.service.security.DTOs.RefreshTokenDTO;
+import com.technew.studiophotografy.service.security.tokens.TokenService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.time.Instant;
 
 @Service
 public class AuthService {
@@ -22,10 +27,12 @@ public class AuthService {
     private final UserRepository userRepository;
     private final RolesRepository rolesRepository;
     private final TokenService tokenService;
-    public AuthService(UserRepository userRepository, RolesRepository rolesRepository, TokenService tokenService) {
+    private final RefreshTokenRepository refreshTokenRepository;
+    public AuthService(UserRepository userRepository, RolesRepository rolesRepository, TokenService tokenService, RefreshTokenRepository refreshTokenRepository) {
         this.userRepository = userRepository;
         this.rolesRepository = rolesRepository;
         this.tokenService = tokenService;
+        this.refreshTokenRepository = refreshTokenRepository;
     }
 
 
@@ -36,9 +43,13 @@ public class AuthService {
         var usernamePassword= new UsernamePasswordAuthenticationToken(authUserDTO.userEmail(), authUserDTO.userPassword());
         this.authenticationManager.authenticate(usernamePassword);
 
-        LoginResponse response = tokenService.tokenGenerate(user);
+        String acessToken = tokenService.generateAccessToken(user);
+        String refreshToken = tokenService.generateRefreshToken(user);
 
-        return ResponseEntity.ok(response);
+
+        return ResponseEntity.ok(
+                new LoginResponse(acessToken, refreshToken)
+        );
     }
 
 
@@ -60,7 +71,24 @@ public class AuthService {
 
     }
 
+    public ResponseEntity<LoginResponse> refresh(RefreshTokenDTO refreshTokenDTO){
+        var tokenRefresh = refreshTokenRepository.findByToken(refreshTokenDTO.refreshToken())
+                .orElseThrow(() -> new RuntimeException("Refresh token Invalido"));
+        if (tokenRefresh.getExpiresAt().isBefore(Instant.now())){
+            throw new RuntimeException("Refresh token expirado");
+        }
 
+        Users user = tokenRefresh.getUsers();
+        refreshTokenRepository.delete(tokenRefresh);
+        String newAcessToken = tokenService.generateAccessToken(user);
+        String newRefreshToken = tokenService.generateRefreshToken(user);
+
+
+
+        return ResponseEntity.ok(new LoginResponse(
+                newAcessToken, newRefreshToken
+        ));
+    }
 
 
 
