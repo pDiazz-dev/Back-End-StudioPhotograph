19a8bb39722e5ff985767e9c56ca30ddbd1af6e6
Pedro <pedroestudos20252025@gmail.com>
2026-03-10 17:11:29 -0300

    feat(TokenCleanUpService):service for delete refresh tokens already expires

--- src/main/java/com/technew/studiophotografy/service/security/tokens/TokenCleanUpService.java
+++ src/main/java/com/technew/studiophotografy/service/security/tokens/TokenCleanUpService.java
@@ -1,4 +1,23 @@
 package com.technew.studiophotografy.service.security.tokens;
 
-public class TokenCleanUp {
+import com.technew.studiophotografy.repository.RefreshTokenRepository;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+
+@Service
+public class TokenCleanUpService {
+
+    private final RefreshTokenRepository refreshTokenRepository;
+
+    public TokenCleanUpService(RefreshTokenRepository refreshTokenRepository) {
+        this.refreshTokenRepository = refreshTokenRepository;
+    }
+
+    @Scheduled(fixedRate = 86400000) // -> executar sempre a cada 24 horas
+    public void deleteExpiresAtToken(){
+        refreshTokenRepository.deleteByExpiresAtBefore(Instant.now());
+    }
+
 }
